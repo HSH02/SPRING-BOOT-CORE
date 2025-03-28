db971303cd7149f0da9f44f8c34ea7e403c08416
HSH02 <tkrhktkdwk10@gmail.com>
2025-03-28 23:28:31 +0900

    commit test

--- src/main/java/com/example/springcore/annotations/example/ControllerTest.java
+++ src/main/java/com/example/springcore/annotations/example/ControllerTest.java
@@ -1,7 +1,5 @@
 package com.example.springcore.annotations.example;
 
-import org.springframework.stereotype.Controller;
-import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,9 +7,8 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class ControllerTest {
 
-
-    @RequestMapping(value = "/items", method = RequestMethod.GET)
-    public void getItems() {
-        // 아이템 목록 반환
-    }
+	@RequestMapping(value = "/items", method = RequestMethod.GET)
+	public void getItems() {
+		// 아이템 목록 반환
+	}
 }
